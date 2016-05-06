39357c46c44c5a5a7cd20b132f908f5c135e7d37
yuri <yuri@ifalo.com.tw>
2016-05-06 11:01:07 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var elixir = require('laravel-elixir');
 
 /*
  |--------------------------------------------------------------------------
- | Elixir Asset Management
+ | Elixir Asset Management.
  |--------------------------------------------------------------------------
  |
  | Elixir provides a clean, fluent API for defining some basic Gulp tasks
